a76519327abd90af8345772335fae36d736589b0
Maijs Garais <maireadsbooks@gmail.com>
2022-09-25 21:45:55 +0100

    check if grounded with boxcast not manually, fix gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     // refs
-    private Rigidbody2D _body;
-    private Animator    _anim;
+    private Rigidbody2D     _body;
+    private Animator        _anim;
+    private BoxCollider2D   _boxCollider;
 
     // vars
     [SerializeField] private float _speed;
-    private bool _isGrounded;
+    [SerializeField] private LayerMask _groundLayer;
 
     private void Awake() {
         // set up refs
         _body = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
         // adjust body velocity only horizontally according to input
-        _body.velocity = new Vector2(horizontalInput * _speed, _body.velocity.y);
+        _body.velocity = new Vector2(
+            horizontalInput * _speed, _body.velocity.y);
 
         // turn player around when changing movement direction
         if (horizontalInput > 0.01f) {
@@ -30,24 +33,28 @@ public class PlayerMovement : MonoBehaviour {
         }
 
         // jump when hitting space only if player is on ground
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) {
             Jump();
         }
 
         // set animator params
         _anim.SetBool("isRunning", (horizontalInput != 0));
-        _anim.SetBool("isGrounded", _isGrounded);
+        _anim.SetBool("isGrounded", isGrounded());
     }
 
     private void Jump() {
         _body.velocity = new Vector2(_body.velocity.x, _speed);
-        _isGrounded = false;
         _anim.SetTrigger("Jump");
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Ground") {
-            _isGrounded = true;
         }
+    }
+
+    private bool isGrounded() {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center,
+            _boxCollider.size, 0, Vector2.down, 0.1f, _groundLayer);
+        return raycastHit.collider != null;
     }
 }
