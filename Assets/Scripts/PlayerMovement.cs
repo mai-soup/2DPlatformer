51bb30ebb4db91cbdd2b9728af372ca1c46a3bc1
Maijs Garais <maireadsbooks@gmail.com>
2022-09-25 17:34:45 +0100

    add player sprite, set up animations for player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     // refs
-    private Rigidbody2D body;
+    private Rigidbody2D _body;
+    private Animator    _anim;
 
     // vars
-    [SerializeField] private float speed;
+    [SerializeField] private float _speed;
+    private bool _isGrounded;
 
     private void Awake() {
-        body = GetComponent<Rigidbody2D>();
+        // set up refs
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update() {
+        float horizontalInput = Input.GetAxis("Horizontal");
         // adjust body velocity only horizontally according to input
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        _body.velocity = new Vector2(horizontalInput * _speed, _body.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            body.velocity = new Vector2(body.velocity.x, speed);
+        // turn player around when changing movement direction
+        if (horizontalInput > 0.01f) {
+            transform.localScale = Vector3.one;
+        } else if (horizontalInput < -0.01f) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // jump when hitting space only if player is on ground
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
+            Jump();
+        }
+
+        // set animator params
+        _anim.SetBool("isRunning", (horizontalInput != 0));
+        _anim.SetBool("isGrounded", _isGrounded);
+    }
+
+    private void Jump() {
+        _body.velocity = new Vector2(_body.velocity.x, _speed);
+        _isGrounded = false;
+        _anim.SetTrigger("Jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
+            _isGrounded = true;
         }
     }
 }
