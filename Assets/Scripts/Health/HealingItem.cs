51543e68a9d5eacc531d260e2078361ef735126f
Maijs Garais <maireadsbooks@gmail.com>
2022-11-16 22:18:51 +0000

    add healing item pickup sound

--- Assets/Scripts/Health/HealingItem.cs
+++ Assets/Scripts/Health/HealingItem.cs
@@ -6,9 +6,13 @@ public class HealingItem : MonoBehaviour {
     // vars
     [SerializeField] private float _hpValue;
 
+    [Header("Sound")]
+    [SerializeField] private AudioClip _pickupSound;
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
             collision.GetComponent<Health>().RestoreHealth(_hpValue);
+            SoundManager.instance.PlaySound(_pickupSound);
             gameObject.SetActive(false);
         }
     }
